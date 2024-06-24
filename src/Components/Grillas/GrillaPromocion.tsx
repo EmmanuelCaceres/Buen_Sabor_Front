8aa2a8a6f5f84f9dea1d6aa1c2d76bab80c67f74
ValentinBaldini <valentinbaldini28@gmail.com>
2024-06-24 17:43:29 -0300

    Arreglo para vercel

--- src/Components/Grillas/GrillaPromocion.tsx
+++ src/Components/Grillas/GrillaPromocion.tsx
@@ -2,15 +2,14 @@ import { useEffect, useState } from "react";
 import IPromocion from "../../Entities/IPromocion";
 import PromocionService from "../../Functions/Services/PromocionService";
 import { Link } from "react-router-dom";
-import CardPromocionDashboard from "../CardPromocionDashboard";
-import NavBar from "../NavBar";
+/*import CardPromocionDashboard from "../CardPromocionDashboard";*/
 
 export default function GrillaPromocion() {
     
     const apiUrl = import.meta.env.VITE_URL_API_BACK
    
 
-    const [promociones, setPromociones] = useState<IPromocion[]>([]);
+    const [/*promociones*/, setPromociones] = useState<IPromocion[]>([]);
 
     const mostrarDatos = (url: string) => {
         const result = new PromocionService(url);
@@ -25,7 +24,7 @@ export default function GrillaPromocion() {
 
     useEffect(() => {
         mostrarDatos(`${apiUrl}empresas/includeDeleted`)
-    }, [])
+    }, [apiUrl])
 
     return(
         <>
@@ -36,10 +35,10 @@ export default function GrillaPromocion() {
                         Añadir empresa
             </Link> */}
             <div className="containerCardEmpresa">
-                {
+                {/*
                     promociones && promociones.map((promocion: IPromocion, key = promocion.id) => (
-                        <CardPromocionDashboard key={key} data={promocion}></CardPromocionDashboard>
-                    ))
+                        /*<CardPromocionDashboard key={key} data={promocion}></CardPromocionDashboard>
+                    ))*/
                 }
                 <Link to={'save/0'} className="cardEmpresa cardEmpresaSave">
                     Agregar Promoción
--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -14,7 +14,7 @@ export default function NavBar() {
         <CNavbar className="bg-dark fixed-top">
             <CContainer fluid className="d-flex justify-content-between align-items-center">
                 <div className="d-flex align-items-center">
-                    <img src={logoImage} alt="Logo" style={{ height: "auto", width: "50px", marginLeft: "2px", color: "white" }} />
+                    <img src={logoImage} alt="Logo" style={{ height: "auto", width: "45px", marginLeft: "15px", color: "white" }} />
                     <CNavbarBrand href="#" className="ms-3" style={{ fontFamily: 'Arial, sans-serif', fontSize: '1rem', color: '#c6c9de', fontWeight: 'bold' }}>
                         EL BUEN SABOR
                     </CNavbarBrand>
